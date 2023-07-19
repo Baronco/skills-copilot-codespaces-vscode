217c4d13baaa21b6f6f4dc779f1840c31864c99f
Baronco <vorunplz@gmail.com>
2023-07-18 23:00:46 -0500

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const hostname = '127.0.0.1'; // Use '0.0.0.0' to listen on all available network interfaces
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
